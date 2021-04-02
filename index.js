feefdccdcb3a2d389d76bea7d67a9c2b4b2ccd40
Farihaakter <yellowfariha@gmail.com>
2021-04-02 11:52:31 +0600

    added changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 5055
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssth5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World fariha!')
